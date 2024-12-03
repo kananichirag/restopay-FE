48b239e9b329e9f122b232a76b2631debb19c6c1
kananichirag <chiragkanani144@gmail.com>
2024-12-03 21:03:18 +0530

    Change Header style

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,32 +5,26 @@ function Header() {
     <div>
       <div className="flex justify-around items-center h-[80px] px-6 ">
         <div>
-          <h1 className="text-2xl font-bold text-gray-500">OrderNest</h1>
+          <h1 className="text-5xl font-bold text-red-500">RestoPay</h1>
         </div>
-        <div className="flex space-x-6">
-          <div className="hover:bg-slate-200 p-1 rounded transition duration-200">
-            <a href="#" className="text-gray-700 hover:text-blue-500">
+        <div className="flex space-x-10">
+          <div className="hover:bg-red-100 text-gray-700 hover:text-red-700 p-2  rounded transition duration-200 cursor-pointer">
+            <a href="#" className="">
               Billing
             </a>
           </div>
-          <div className="hover:bg-slate-200  p-1 rounded transition duration-200">
-            <a href="#" className="text-gray-700 hover:text-blue-500">
-              Inventory
-            </a>
+          <div className="hover:bg-red-100 text-gray-700 hover:text-red-700 p-2 rounded transition duration-200 cursor-pointer">
+            <a href="#">Inventory</a>
           </div>
-          <div className="hover:bg-slate-200  p-1 rounded transition duration-200">
-            <a href="#" className="text-gray-700 hover:text-blue-500">
-              Reporting
-            </a>
+          <div className="hover:bg-red-100 text-gray-700 hover:text-red-700 p-2 rounded transition duration-200 cursor-pointer">
+            <a href="#">Reporting</a>
           </div>
-          <div className="hover:bg-slate-200  p-1 rounded transition duration-200">
-            <a href="#" className="text-gray-700 hover:text-blue-500">
-              Menu
-            </a>
+          <div className="hover:bg-red-100 text-gray-700 hover:text-red-700 p-2 rounded transition duration-200 cursor-pointer">
+            <a href="#">Menu</a>
           </div>
         </div>
         <div>
-          <button className="bg-blue-500 text-white px-6 py-2 rounded-lg hover:bg-blue-600 transition duration-200">
+          <button className="bg-red-500 text-white px-6 py-2 rounded-lg hover:bg-red-600 transition duration-200">
             Book Demo
           </button>
         </div>
